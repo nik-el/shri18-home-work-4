a4cdd9da3a80903b237515df522048bf4dabf7fb
Nikita Litvinenko <nik-el@mail.ru>
2018-11-02 16:41:59 +0300

    add eslint

---
+++ .eslintrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  "extends": ["eslint:recommended", "plugin:node/recommended"],
+  "globals": {
+    "XMLHttpRequest": false,
+    "alert": false,
+  },
+  "rules": {
+    "semi": 0,
+    "no-console":0,
+  }
+};
